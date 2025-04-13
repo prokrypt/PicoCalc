ec0a5bb74d31bf1795ecda9f34199380f6a7feb5
prokrypt <prokrypt@edited.us>
2025-04-12 21:12:44 -0700

    Update backlight.ino

--- Code/picocalc_keyboard/backlight.ino
+++ Code/picocalc_keyboard/backlight.ino
@@ -1,10 +1,6 @@
 
 #include "backlight.h"
 
-#define KBD_BACKLIGHT_SEG 4
-uint8_t kbd_backlight_segs[KBD_BACKLIGHT_SEG] = {20,40,80,110};
-uint8_t kbd_backlight_offset = 0;
-
 void lcd_backlight_update_reg() {
 
   uint8_t val;
@@ -12,46 +8,55 @@ void lcd_backlight_update_reg() {
   val = reg_get_value(REG_ID_BKL);
   val = val & 0xff;
   
-  analogWriteFrequency(10000); 
+  analogWriteFrequency(10000);
   analogWrite(PA8, val);
 
 }
 
 void lcd_backlight_update(int v) {
-  int val;
+    uint8_t val = reg_get_value(REG_ID_BKL);
 
-  val = reg_get_value(REG_ID_BKL);
-  val += v;
-  if(val < 0) val = 0;
-  if(val > 0xff) val = 0xff;
- 
-  analogWriteFrequency(10000); 
-  analogWrite(PA8, val);  
-  reg_set_value(REG_ID_BKL,val);
+    if (v > 0) {
+        if (val < 1)
+            val = 1;
+        else {
+            val *= 2;
+            if (val > 255)
+              val = 255;
+        }
+    } else if (v < 0) {
+        if (val > 1)
+            val /= 2;
+    }
+
+    analogWriteFrequency(10000);
+    analogWrite(PA8, val);
+    reg_set_value(REG_ID_BKL, val);
 }
 
-void kbd_backlight_update(int v){
-  int val;
+void kbd_backlight_update_reg(){
+
+  uint8_t val;
 
   val = reg_get_value(REG_ID_BK2);
-  val += v;
-  if(val < 20 ) val = 0;
-  if(val > 0xff) val = 0;
- 
+  val = val & 0xff;
+
   analogWriteFrequency(10000); 
-  analogWrite(PC8, val);  
-  reg_set_value(REG_ID_BK2,val);
+  analogWrite(PC8, val);
+
 }
 
 void kbd_backlight_update_offset(){
   int val;
-  kbd_backlight_offset++;
-  kbd_backlight_offset =  kbd_backlight_offset % KBD_BACKLIGHT_SEG;
-  
   val = reg_get_value(REG_ID_BK2);
-  val += kbd_backlight_segs[kbd_backlight_offset];
-  if(val < 20 ) val = 0;
-  if(val > 0xff) val = 0;
+  if(val == 0 )
+    val = 16;
+  else
+    val *= 2;
+  if (val > 200)
+    val = 0;
+  else if(val >110)
+    val = 110;
  
   analogWriteFrequency(10000); 
   analogWrite(PC8, val);  
